5ba76e24b392f39196af7a43bc5e855acc0ae222
Ensar Spahiu <spahiensar@gmail.com>
2023-09-18 20:02:50 +0200

    Updated logic for quickness

--- server.js
+++ server.js
@@ -8,37 +8,39 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+async function sendEmail(name, email, message, interests, res) {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'spahiuensar@gmail.com',
+        /* Generated password */
+        pass: 'rjncgudldidxltgs',
+      },
+    });
+
+    const mailOptions = {
+      /* Sender email */
+      from: 'spahiuensar@gmail.com',
+      /* Recipient email */
+      to: 'contact@nexiphor.com,dorantdushi@gmail.com,ensar.spahiu@hotmail.com',
+      subject: 'Contact Form Submission',
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nInterests: ${interests}`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+    res.send('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email');
+  }
+}
+
 /* Route to handle sending emails */
 app.post('/send-email', (req, res) => {
   const { name, email, message, interests } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'spahiuensar@gmail.com',
-      /* Generated password */
-      pass: 'rjncgudldidxltgs'
-    }
-  });
-
-  const mailOptions = {
-    /* Sender email */
-    from: 'spahiuensar@gmail.com',
-    /* Recipient email */
-    to: 'contact@nexiphor.com,dorantdushi@gmail.com,ensar.spahiu@hotmail.com',
-    subject: 'Contact Form Submission',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nInterests: ${interests}`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending email:', error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent:', info.response);
-      res.send('Email sent successfully');
-    }
-  });
+  sendEmail(name, email, message, interests, res);
 });
 
 /* Start the server */
